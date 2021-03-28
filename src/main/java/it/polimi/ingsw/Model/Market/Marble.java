205f1502afe91cd697a841691c45446e74d48396
stefanofossati <10569836@polimi.it>
2021-03-28 16:49:18 +0200

    add Market class and testMarket class

---
+++ src/main/java/it/polimi/ingsw/Model/Market/Marble.java
@@ -0,0 +1,57 @@
+package it.polimi.ingsw.Model.Market;
+
+/**
+ * Enumeration of the marble, the Market could use six type of marble.
+ * This Enumeration is used by the Market to manage the position of each marble
+ * and to return the type of marble chose by the player.
+ *
+ * @author Stefano Fossati
+ */
+
+public enum Marble {
+    /**
+     *
+     */
+    FAITH("faith"),
+
+    /**
+     *
+     */
+    GOLD("gold"),
+
+    /**
+     *
+     */
+    NOTHING("nothing"),
+
+    /**
+     *
+     */
+    SERVANT("servant"),
+
+    /**
+     *
+     */
+    SHIELD("shield"),
+
+    /**
+     *
+     */
+    STONE("stone");
+
+
+    private String marbleType;
+
+    Marble(String marbleType){
+        this.marbleType = marbleType;
+
+    }
+
+    /**
+     * Getter that returns the type of the marble
+     * @return The type of the marble
+     */
+    public String getType(){
+        return this.marbleType;
+    }
+}
