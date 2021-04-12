a0d718d349533efabf2fc72a0c3dac773ff62ea6
stefanofossati <10569836@polimi.it>
2021-04-12 17:42:48 +0200

    add single player

---
+++ src/main/java/it/polimi/ingsw/Model/Game/SinglePlayerGame.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.Model.Game;
+
+import it.polimi.ingsw.Model.DevelopmentCard.DevelopmentCard;
+import it.polimi.ingsw.Model.Exceptions.StartGameException;
+import it.polimi.ingsw.Model.Lorenzo.LorenzoIlMagnifico;
+
+import java.util.ArrayList;
+
+public class SinglePlayerGame extends Game{
+    Player player;
+    LorenzoIlMagnifico lorenzoIlMagnifico;
+
+    public SinglePlayerGame(String name) {
+        super(1);
+        this.lorenzoIlMagnifico = new LorenzoIlMagnifico();
+        this.player = new Player(name);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public LorenzoIlMagnifico getLorenzoIlMagnifico() {
+        return lorenzoIlMagnifico;
+    }
+
+    @Override
+    public ArrayList[][] getDevelopmentCards() {
+        return super.getDevelopmentCards();
+    }
+
+    @Override
+    public DevelopmentCard[][] getDevelopmentCardsAvailable() {
+        return super.getDevelopmentCardsAvailable();
+    }
+
+    @Override
+    public DevelopmentCard buyDevelopmentCard(int color, int level) throws StartGameException {
+        return super.buyDevelopmentCard(color, level);
+    }
+
+    @Override
+    public void startGame() throws StartGameException {
+        super.startGame();
+    }
+}
