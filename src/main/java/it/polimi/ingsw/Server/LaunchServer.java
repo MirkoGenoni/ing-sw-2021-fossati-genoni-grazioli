24cea174958cb85145ce42dfebaa7f0b8c907d41
stefanofossati <10569836@polimi.it>
2021-06-20 11:30:17 +0200

    fix error Server

--- src/main/java/it/polimi/ingsw/Server/LaunchServer.java
+++ src/main/java/it/polimi/ingsw/Server/LaunchServer.java
@@ -1,14 +1,9 @@
 package it.polimi.ingsw.Server;
 
-import java.io.IOException;
 
 public class LaunchServer {
     public static void main(String[] args) {
-        try{
-            Server server = new Server();
-            new Thread(server).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server();
+        new Thread(server).start();
     }
 }
